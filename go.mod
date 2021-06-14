d85e862b49f2c1d1b583b29b41b8c3348b0a37c9
Donald King <chronos@chronos-tachyon.net>
2021-06-13 19:32:50 -0700

    Initial code commit.

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/chronos-tachyon/syncrand
+
+go 1.16
+
+require github.com/chronos-tachyon/assert v1.2.0
---
+++ syncrand.go
@@ -0,0 +1,135 @@
+// Package syncrand provides synchronized (thread-safe) wrappers of
+// "math/rand".Source64 and "math/rand".Rand.
+//
+package syncrand
+
+import (
+	"math/rand"
+	"sync"
+	"time"
+
+	"github.com/chronos-tachyon/assert"
+)
+
+var (
+	gMu     sync.Mutex
+	gSource rand.Source64
+	gRand   *rand.Rand
+)
+
+func init() {
+	gSource = NewSource(time.Now().UnixNano())
+	gRand = rand.New(gSource)
+}
+
+// WrappedSource represents a source of randomness which is itself backed by
+// another source of randomness.  The Unwrap() method provides access to the
+// underlying Source.
+type WrappedSource interface {
+	rand.Source64
+	Unwrap() rand.Source64
+}
+
+// Global returns this package's global instance of Rand.  It is guaranteed to
+// be thread-safe.
+func Global() *rand.Rand {
+	gMu.Lock()
+	rng := gRand
+	gMu.Unlock()
+	return rng
+}
+
+// GlobalSource returns this package's global instance of Source64, which backs
+// the Rand returned by Global().  It is guaranteed to be thread-safe.
+func GlobalSource() rand.Source64 {
+	gMu.Lock()
+	source := gSource
+	gMu.Unlock()
+	return source
+}
+
+// SetGlobalSource atomically replaces the global instance of Source64 with the
+// provided one.  If it is not thread-safe, then it is wrapped in an
+// implementation of WrappedSource that _is_ thread-safe.
+func SetGlobalSource(source rand.Source64) {
+	gMu.Lock()
+	gSource = WrapSource(source)
+	gRand = rand.New(gSource)
+	gMu.Unlock()
+}
+
+// New returns a new Rand seeded with the given seed.  It is guaranteed to be
+// thread-safe.
+func New(seed int64) *rand.Rand {
+	return rand.New(NewSource(seed))
+}
+
+// NewFromSource returns a new Rand that is backed by the given source.  It is
+// guaranteed to be thread-safe.
+func NewFromSource(source rand.Source64) *rand.Rand {
+	return rand.New(WrapSource(source))
+}
+
+// NewSource returns a new Source64 that is seeded with the given seed.  It is
+// guaranteed to be thread-safe.
+func NewSource(seed int64) rand.Source64 {
+	return WrapSource(rand.NewSource(seed).(rand.Source64))
+}
+
+// WrapSource returns an instance of Source64 that is thread-safe.  It may
+// return its argument unchanged, if that argument is already known to be
+// thread-safe.
+func WrapSource(source rand.Source64) rand.Source64 {
+	if IsThreadSafe(source) {
+		return source
+	}
+	return &syncSource{s64: source}
+}
+
+// IsThreadSafe returns true if its argument is known to be thread-safe.
+func IsThreadSafe(source rand.Source64) bool {
+	assert.NotNil(&source)
+
+	for source != nil {
+		if _, ok := source.(*syncSource); ok {
+			return true
+		}
+
+		wrapper, ok := source.(WrappedSource)
+		if !ok {
+			return false
+		}
+
+		source = wrapper.Unwrap()
+	}
+	return false
+}
+
+type syncSource struct {
+	mu  sync.Mutex
+	s64 rand.Source64
+}
+
+func (ss *syncSource) Seed(seed int64) {
+	ss.mu.Lock()
+	defer ss.mu.Unlock()
+	ss.s64.Seed(seed)
+}
+
+func (ss *syncSource) Int63() int64 {
+	ss.mu.Lock()
+	defer ss.mu.Unlock()
+	return ss.s64.Int63()
+}
+
+func (ss *syncSource) Uint64() uint64 {
+	ss.mu.Lock()
+	defer ss.mu.Unlock()
+	return ss.s64.Uint64()
+}
+
+func (ss *syncSource) Unwrap() rand.Source64 {
+	return ss.s64
+}
+
+var _ rand.Source64 = (*syncSource)(nil)
